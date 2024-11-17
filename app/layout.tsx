ce6d83d9b49e0c759fabf81895660ebfaa72f677
Aayush <aayushg2908@gmail.com>
2024-11-17 20:59:25 +0530

    imp: top loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Toaster } from "sonner";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <NextTopLoader color="#8E24AA" />
           <Toaster richColors />
           {children}
         </body>
